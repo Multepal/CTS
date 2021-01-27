aed41ecfe054902db8ce09d80a7a7ee313b399da
winnieepm <>
2021-01-26 19:43:47 -0500

    right docs
    replaced wrong edition docs with correct ones

--- escolios/escolios.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python3
-
-import xml.etree.ElementTree as ET
-import glob
-
-for xmlfile in sorted(glob.glob('./xom-escolios-*.xml')):
-    print(xmlfile)
-    tree = ET.parse(xmlfile)
-    #root = tree.getroot()
